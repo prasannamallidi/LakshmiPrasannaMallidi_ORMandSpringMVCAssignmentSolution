d4dd7f532e3418c6691491bf548496a9979a16f3
prasannamallidi <prasanna.m90@gmail.com>
2022-07-07 18:22:04 +0530

    Create CustomerController.java

---
+++ src/main/java/com/crm/controller/CustomerController.java
@@ -0,0 +1,63 @@
+package com.crm.controller;
+
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.crm.model.Customer;
+import com.crm.service.CustomerService;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	@RequestMapping("/list")
+	public String listCustomers(Model theModel) {
+		List<Customer> theCustomers = customerService.findAll();
+		theModel.addAttribute("Customers",theCustomers);
+		return "list-Customers";
+		
+	}
+	
+	@RequestMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		Customer theCustomer = new Customer();
+		theModel.addAttribute("Customer",theCustomer);
+		return "Customer-form";
+	}
+	
+	@RequestMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId") int theId, Model theModel) {
+		Customer theCustomer = customerService.findById(theId);
+		theModel.addAttribute("Customer",theCustomer);
+		return "Customer-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveStudent(@RequestParam("id") int id, @RequestParam("firstName") String firstName,@RequestParam("lastName") String lastName, @RequestParam("email") String email) {
+		Customer theCustomer;
+		if(id != 0) {
+			theCustomer = customerService.findById(id);
+			theCustomer.setFirstName(firstName);
+			theCustomer.setLastName(lastName);
+			theCustomer.setEmail(email);
+		}
+		else
+		{
+			theCustomer = new Customer(firstName, lastName, email);
+			
+		}
+		customerService.save(theCustomer);
+		return "redirect:/customer/list";
+	}
+
+}
+
