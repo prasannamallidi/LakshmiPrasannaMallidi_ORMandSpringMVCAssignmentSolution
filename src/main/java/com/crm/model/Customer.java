d6e07ce51f8de7e7ee395625ea45102593778896
prasannamallidi <prasanna.m90@gmail.com>
2022-07-07 18:23:21 +0530

    Create Customer.java

---
+++ src/main/java/com/crm/model/Customer.java
@@ -0,0 +1,70 @@
+package com.crm.model;
+
+import javax.persistence.Column;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customer")
+public class Customer {
+	@Id
+	@Column(name = "customerId")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name = "firstName")
+	private String firstName;
+	@Column(name = "lastName")
+	private String lastName;
+	@Column(name = "email")
+	private String email;
+	
+	public Customer(String firstname,String lastName, String email) {
+		this.firstName = firstname;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+	public Customer() {
+		
+	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+}
